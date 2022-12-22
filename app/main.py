babbc429cddbc2ec8f98a3992ee821a1f482194b
AmirMasoud Nourollah <Nourollah.am@gmail.com>
2022-12-22 19:08:43 +0330

    [UPDATE] Middleware Added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models, schemas, database
 from .routers import post, user, auth, vote
@@ -7,6 +8,12 @@ from .routers import post, user, auth, vote
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   expose_headers=["Content-Disposition"])
 
 app.include_router(post.router)
 app.include_router(user.router)
@@ -14,9 +21,9 @@ app.include_router(auth.router)
 app.include_router(vote.router)
 
 
-@app.get('/', response_model=schemas.Post)
+@app.get('/')
 async def hello_app():
-    return {'message': 'Hello, world'}
+    return {'message': 'Hello to FastAPI Social Media App'}
 
 
 
