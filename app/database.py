3628c92a5e288a001f6e73dac0e13e683c723a76
AmirMasoud Nourollah <Nourollah.am@gmail.com>
2022-12-23 03:28:14 +0330

    [FEATURE] Throw Project to Docker

--- app/database.py
+++ app/database.py
@@ -3,9 +3,8 @@ from sqlalchemy.ext import declarative
 from urllib.parse import quote
 from .config import settings
 
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{quote(settings.database_password)}' \
-                          f'@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{quote(settings.database_password)}' \
+                          f'@{settings.database_hostname}:{settings.database_port / {settings.database_name} }'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
